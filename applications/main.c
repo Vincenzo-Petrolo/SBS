afef342267af492705ec0c3c37e9043cb8bd0652
Vincenzo Petrolo <vincenzo.petrolo99@gmail.com>
2022-01-13 22:36:59 +0100

    feat : added configuration file with defines
    with this commit it is introduced the DEBUG_LEVEL keywork

--- applications/main.c
+++ applications/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "sbs_configuration.h"
 
 int main(void)
 {
@@ -8,4 +9,3 @@ int main(void)
     printf("saluti viandante\n");
     return 0;
 }
-
---
+++ applications/sbs_configuration.h
@@ -0,0 +1,16 @@
+#ifndef __SBS_CONFIGURATION__
+#define __SBS_CONFIGURATION__
+
+
+/*During development use the DEBUG_LEVEL so that it can be disabled
+ * globally just by changing this parameter.
+ *
+ * LEVEL 3 : Heavy debugging
+ * LEVEL 2 : Moderate debugging
+ * LEVEL 1 : Light debugging
+ * LEVEL 0 : No debugging at all
+ * */
+
+#define DEBUG_LEVEL 3
+
+#endif
