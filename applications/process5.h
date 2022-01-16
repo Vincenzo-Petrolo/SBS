0f1c5e9e015a8394b271539b527bda9f405648e5
AlfredoPaolino <paolinoalfre@gmail.com>
2022-01-16 13:01:10 +0100

    feat : added (random) sending values to process 5

--- applications/process5.h
+++ applications/process5.h
@@ -11,7 +11,15 @@
 #define P5_DEADLINE 10 //ms
 #define P5_MB_POOL_SIZE 128
 
+typedef struct {
+  uint8_t value;
+  uint8_t sensor;
+} msg_t;
 
+int rpm_comp();
+int vel_comp();
+int hum_comp();
+int prox_comp();
 
 void process5_entry(void *parameter)
 {
@@ -27,10 +35,96 @@ void process5_entry(void *parameter)
 
     while (1) {
 
+        rpm = rpm_comp();
+        vel = vel_comp();
+        hum = hum_comp();
+        prox = prox_comp();
 
+        if (rpm != -1) {
+            msg.value = rpm;
+            msg.sensor = 'R';
+            result = rt_mb_send(&p6_mailbox, (rt_uint32_t)&msg);
+            DEBUG_PRINT("Process 5 is sending a mail\n", HEAVY_DEBUG);
+        }
+
+        if (vel != -1) {
+            msg.value = vel;
+            msg.sensor = 'V';
+            result = rt_mb_send(&p6_mailbox, (rt_uint32_t)&msg);
+            result = rt_mb_send(&p8_mailbox, (rt_uint32_t)&msg);
+            DEBUG_PRINT("Process 5 is sending a mail\n", HEAVY_DEBUG);
+        }
+
+        if (hum != -1) {
+            msg.value = hum;
+            msg.sensor = 'H';
+            result = rt_mb_send(&p8_mailbox, msg);
+            DEBUG_PRINT("Process 5 is sending a mail\n", HEAVY_DEBUG);
+        }
+
+        if (prox != -1) {
+            msg.value = prox;
+            msg.sensor = 'P';
+            result = rt_mb_send(&p8_mailbox, msg);
+            //DEBUG_PRINT("Process 5 is sending a mail\n", HEAVY_DEBUG);
+        }
+
+
+        if (result != RT_EOK) {
+            DEBUG_PRINT("Process5 wasn't able to send mail\n",LIGHT_DEBUG);
+            /*Continue with next cycle*/
+            continue;
+        }
 
     return;
 }
 
+    int rpm_comp() {
+        int v;
+
+        srand(time(NULL));
+
+        v=rand%10;
+        if (v ==0)
+            return rand%401;
+        else
+            return -1;
+    }
+
+    int vel_comp() {
+        int v;
+
+        srand(time(NULL));
+
+        v=rand%10;
+        if (v ==0)
+            return rand%81;
+        else
+            return -1;
+    }
+
+    int hum_comp() {
+        int v;
+
+        srand(time(NULL));
+
+        v=rand%10;
+        if (v ==0)
+            return rand%101;
+        else
+            return -1;
+    }
+
+    int prox_comp() {
+        int v;
+
+        srand(time(NULL));
+
+        v=rand%10;
+        if (v ==0)
+            return rand%21;
+        else
+            return -1;
+    }
 
 #endif
